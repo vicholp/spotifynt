8938ce75298e97e3f2421f7bc7bd9d32535ad8c1
vicholpe <vlinerospardo@gmail.com>
2022-02-05 03:32:50 -0300

    feat: migration with tags for tracks

--- database/migrations/2022_01_30_172127_create_tracks_table.php
+++ database/migrations/2022_01_30_172127_create_tracks_table.php
@@ -17,11 +17,33 @@ class CreateTracksTable extends Migration
             $table->id();
             $table->timestamps();
 
-            $table->string('mb_id', 500)->unique();
+            $table->string('mb_id', 500)->unique()->nullable();
             $table->integer('beets_id');
             $table->string('name', 1000);
             $table->string('path', 1000);
             $table->foreignId('album_id')->constrained();
+            $table->integer('track_position')->nullable();
+
+            $table->integer('length');
+            $table->string('format', 100);
+            $table->integer('bitrate');
+            $table->string('bit_depht', 100);
+            $table->string('sample_rate', 100);
+
+            $table->float('average_loudness')->nullable();
+            $table->integer('bpm')->nullable();
+            $table->float('danceable')->nullable();
+            $table->string('genre_rosamerica', 100)->nullable();
+            $table->string('language', 10)->nullable();
+            $table->float('mood_acoustic')->nullable();
+            $table->float('mood_aggressive')->nullable();
+            $table->float('mood_electronic')->nullable();
+            $table->float('mood_happy')->nullable();
+            $table->float('mood_party')->nullable();
+            $table->float('mood_relaxed')->nullable();
+            $table->float('mood_sad')->nullable();
+            $table->string('moods_mirex', 100)->nullable();
+            $table->string('voice_instrumental', 100)->nullable();
 
             $table->jsonb('beets_tags')->nullable();
         });

4a46dd5cefe7e8c871ec0643bc61aecc53b449a7
vicholpe <vlinerospardo@gmail.com>
2022-02-05 03:33:06 -0300

    chore: add migrations for users and servers

--- database/migrations/2022_01_30_170000_create_beets_servers_table.php
+++ database/migrations/2022_01_30_170000_create_beets_servers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBeetsServersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('beets_servers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
 
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('token_listenbrainz');
-
-            $table->rememberToken();
+            $table->string('name', 500);
+            $table->ipAddress('ip');
         });
     }
 
@@ -34,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('beets_servers');
     }
 }
--- database/migrations/2022_02_05_020952_create_beets_server_user_table.php
+++ database/migrations/2022_02_05_020952_create_beets_server_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBeetsServerUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('beets_server_user', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
 
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('token_listenbrainz');
-
-            $table->rememberToken();
+            $table->foreignId('beets_server_id')->constrained();
+            $table->foreignId('user_id')->constrained();
         });
     }
 
@@ -34,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('beets_server_user');
     }
 }

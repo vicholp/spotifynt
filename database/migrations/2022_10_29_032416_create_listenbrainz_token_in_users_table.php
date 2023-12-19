24071a7f138ad5ef11a900a5e09e925b7feeb703
vicholp <vlinerospardo@gmail.com>
2023-12-19 08:24:54 -0300

    chore: migrations style

--- database/migrations/2022_10_29_032416_create_listenbrainz_token_in_users_table.php
+++ database/migrations/2022_10_29_032416_create_listenbrainz_token_in_users_table.php
@@ -25,7 +25,7 @@ return new class() extends Migration {
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('listenbrainz_token');
         });
     }
 };

297a96ae6417861f87aa176dd0c5761dbe69ade0
vicholp <vlinerospardo@gmail.com>
2025-04-27 23:57:46 -0400

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+        $middleware->validateCsrfTokens(except: [
+            '*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- config/cors.php
+++ config/cors.php
@@ -29,5 +29,4 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => true,
-    'supports_credentials' => true,
 ];

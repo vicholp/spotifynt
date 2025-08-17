adf21b9fb3e5cf5e52719e5e7e7a635895c7b588
vicholp <vlinerospardo@gmail.com>
2025-08-16 22:23:39 -0400

    feat: get files by checksum

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -7,15 +7,22 @@ use App\Http\Requests\UpdateFileRequest;
 use App\Http\Resources\FileCollection;
 use App\Http\Resources\FileResource;
 use App\Models\File;
+use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return new FileCollection(File::all());
+        $files = File::query();
+
+        if ($request->has('checksum_md5')) {
+            $files->where('checksum_md5', $request->input('checksum_md5'));
+        }
+
+        return new FileCollection($files->get());
     }
 
     /**

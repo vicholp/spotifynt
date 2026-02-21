9d368e567b19b01160057cd5fd3a86e4f310bdea
Vicente Lineros Pardo <vlinerospardo@gmail.com>
2026-02-21 23:24:51 +0000

    fix: typo

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('BROADCAST_CONNECTION', 'reberv'),
+    'default' => env('BROADCAST_CONNECTION', 'reberb'),
 
     /*
     |--------------------------------------------------------------------------
